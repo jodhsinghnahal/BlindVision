16244944b8bc1907a94e8d3050892f2278f206d0
jodhsinghnahal <nahal.jodhsingh@gmail.com>
2023-12-27 19:13:14 -0800

    video, speech, api

--- app.py
+++ app.py
@@ -1,23 +1,87 @@
-from flask import Flask, redirect, render_template, request, jsonify
-from OpenSSL import SSL
+from flask import Flask, render_template, request, jsonify
+from ultralytics import YOLO
 import base64
 import io
 from PIL import Image
+import cv2
+import torch
+import google.generativeai as genai
+import os
+import PIL.Image
+from gtts import gTTS
 
 app = Flask(__name__)
-
 
 @app.route("/")
 def main():
     return render_template("camera.html")
 
-
 @app.route("/upload_image", methods=["POST"])
 def image():
     data = request.json
-    print(data)
 
-    return jsonify({'message': 'Image saved successfully'})
+    # Extract the base64-encoded image data
+    image_data = data.get("image", "")
+
+    # Remove the "data:image/jpeg;base64," prefix
+    _, encoded_image = image_data.split(",", 1)
+
+    # Decode the base64-encoded image data
+    image_bytes = base64.b64decode(encoded_image)
+
+    # Convert the image bytes to a PIL Image
+    image = Image.open(io.BytesIO(image_bytes))
+    image = image.transpose(method=Image.FLIP_LEFT_RIGHT)
+
+    # Save the image to a file
+    image.save("uploaded_image.jpg")
+
+    # print(2)
+
+    # # Load YOLOv7 model
+    # # model = torch.hub.load('/yolov7.pt', 'yolov7')
+    # # model = YOLO("yolov7/models/yolo.py")
+    # model = YOLO("yolov7m.pt")
+
+    # print(3)
+
+    # # Load the saved image
+    # image = cv2.imread('uploaded_image.jpg')
+
+    # print(4)
+
+    # # Perform object detection
+    # results = model(image)
+
+    # print(5)
+
+    # # Process detection results
+    # detected_objects = results.pred[0]  # Extract detected objects
+    # for obj in detected_objects:
+    #     print(6)
+    #     label = obj.get_field('names')[0]  # Get label
+    #     confidence = obj.get_field('scores')[0]  # Get confidence score
+    #     box = obj.bbox.int().tolist()  # Get bounding box coordinates
+    #     cv2.rectangle(image, (box[0], box[1]), (box[2], box[3]), (0, 255, 0), 2)
+    #     cv2.putText(image, f'{label}: {confidence:.2f}', (box[0], box[1] - 10),
+    #                 cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+    # # Display the result
+    # cv2.imshow('Object Detection', image)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+
+    # gemini_api_key = os.environ["GOOGLE_API_KEY"]
+    # genai.configure(api_key = gemini_api_key)
+
+    # img = PIL.Image.open('uploaded_image.jpg')
+
+    # model = genai.GenerativeModel('gemini-pro-vision')
+
+    # response = model.generate_content(["what is in this photo", img], stream=True)
+    # response.resolve()
+
+    return jsonify({'message': 'response.text'})
 
 if __name__ == "__main__":
-    app.run(port=4000)
+    app.run(host="0.0.0.0", port=5000, debug=True, threaded=True)
--- static/script.js
+++ static/script.js
@@ -1,19 +1,37 @@
 var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
-const video = document.querySelector("#videoElement");
+const video = document.getElementById("videoElement");
 
 video.width = 400;
 video.height = 300;
 
+var message;
+
+console.log(1)
+let input = document.querySelector('input');
+input.addEventListener('input', function() {
+    console.log(2)
+    x = GetSpeech();
+    // convertToSpeech('hello')
+    var msg = new SpeechSynthesisUtterance();
+    msg.text = message;
+    console.log(message);
+    window.speechSynthesis.speak(msg);
+}); 
+console.log(3)
+
 
 if (navigator.mediaDevices.getUserMedia) {
     navigator.mediaDevices.getUserMedia({
-        video: true
+        audio: false,
+        video: {
+          facingMode: 'environment'
+        }
     })
         .then(function (stream) {
+            console.log(4)
          //resolve stream and set the html src of the video and then play video  
             video.srcObject = stream;
-            console.log("start")
             video.play();
         })
         .catch(function (err0r) {
@@ -23,6 +41,8 @@ if (navigator.mediaDevices.getUserMedia) {
 
 const FPS = 1;
 setInterval(() => {
+    console.log(5)
+
     width = video.width;
     height = video.height;
     context.drawImage(video, 0, 0, width, height);
@@ -37,12 +57,40 @@ setInterval(() => {
         },
         body: JSON.stringify({ image: data }),
     })
-    .then(response => response.json())
+    .then(response => { console.log(response); return response.json()})
     .then(data => {
         console.log('Image sent successfully:', data);
+        message = data['message'];
     })
     .catch(error => {
         console.error('Error sending image:', error);
     });
-}, 10000 / FPS);
+}, 1000 / FPS);
+
+
+function GetSpeech() {
+     console.log("clicked microphone");
+    // var msg = new SpeechSynthesisUtterance();
+    // msg.text = "message";
+    // window.speechSynthesis.speak(msg);
+
+    const SpeechRecognition =  window.SpeechRecognition || window.webkitSpeechRecognition;
+    let recognition = new SpeechRecognition();
+
+        recognition.onstart = () => {
+            console.log("starting listening, speak in microphone");
+        }
+        recognition.onspeechend = () => {
+            console.log("stopped listening");
+            recognition.stop();
+            audio = document.getElementById('audioPlayer');
+            audio.play();
+        }
+        recognition.onresult = (result) => {
+            console.log(result.results[0][0].transcript);
+            document.querySelector('ul').innerHTML = result.results[0][0].transcript;
+            return result.results[0][0].transcript;
+         }
+        recognition.start();
+}
 
